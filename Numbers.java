f6ecec49d6c88ce3a969332c08e419b3ed46b75c
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:20:46 -0700

    added IF/ElSE for the boolean function

--- Numbers.java
+++ Numbers.java
@@ -12,4 +12,17 @@ public class Numbers {
         randomNum = rand.nextInt(101);
     }
 
-  
+  //Boolean for the number guess inputs using IF/ELSE
+public boolean compareNumber(int guess) 
+    {
+        if (randomNum == guess) {
+            System.out.println("Congratulations, you guessed the number!");
+            return true;
+        } else if (randomNum < guess) {
+            System.out.println("I'm sorry. That guess was too high.");
+            return false;
+        } else {
+            System.out.println("I'm sorry, That guess was too low.");
+            return false;
+        }
+    }
