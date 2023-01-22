217eef09d7806081da2c29b3999d441227685646
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:14:25 -0700

    Constructors to Person.java

--- Person.java
+++ Person.java
@@ -7,7 +7,24 @@ public class Person {
     private String lastName;
 
   
+  //Create two overloaded constructors for the Person class
+    // First constructor
+    public Person(String firstName) 
+    {
+        this.firstName = firstName;
+        this.lastName = "";
+    }
 
+    // Second constructor
+    public Person(String firstName, String lastName) 
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+  
+  
+    
   
   
  
