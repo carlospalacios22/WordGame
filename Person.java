8def0a7f42c298c72e1acf3d63d22e8f7e8b6008
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:13:47 -0700

    Variables  to Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,15 @@
 
 //Person class begin
 
+public class Person {
+    // private variables 
+    private String firstName;
+    private String lastName;
+
+  
+
+  
+  
+ 
+  
+}// end of person class
