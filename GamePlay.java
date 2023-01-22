d1f7e795e97f7217f2bab8f2d67d652c17adbb98
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:27:22 -0700

    added loop to Gameplay

--- GamePlay.java
+++ GamePlay.java
@@ -17,3 +17,40 @@ public class GamePlay {
 
         System.out.print("What is your first name? ");
         String firstName = sc.nextLine();
+        
+        //Prompt for the last name if no then it will not get the last name 
+        System.out.print("Would you like to enter a last name? (y/n) ");
+        String lastNameResponse = sc.nextLine();
+        if (lastNameResponse.equalsIgnoreCase("y")) {
+            System.out.print("What is your last name? ");
+            String lastName = sc.nextLine();
+            game.player = new Person(firstName, lastName);
+        } else {
+            game.player = new Person(firstName);
+        }
+        
+            boolean correctGuess = false;
+        
+        //added loop until the number is gussed corrrectly 
+        while (!correctGuess) 
+        {
+            System.out.print(game.player.getFirstName() + ", please enter a guess: ");
+            int guess = sc.nextInt();
+            sc.nextLine();
+            correctGuess = game.randomNumber.compareNumber(guess);
+     
+        }
+        
+        
+        
+    }// end of void main
+    
+    
+}//end of Gameplay
+    
+    
+    
+    
+    
+    
+    
