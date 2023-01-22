1928eff162c04c379c7e0ed4bd6c60bc0f117be8
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:23:09 -0700

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -1,1 +1,19 @@
+//Import Scanner library for input/outputs
+import java.util.Scanner;
 
+//Main Class for the game
+//Gameplay class Start
+
+public class GamePlay {
+    private Person player;
+    private Numbers randomNumber;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GamePlay game = new GamePlay();
+
+        game.randomNumber = new Numbers();
+        game.randomNumber.generateNumber();
+
+        System.out.print("What is your name? ");
+        String firstName = sc.nextLine();
