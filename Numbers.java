b85cfba74062489a79ccfa3ced494cbd88c8846a
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-21 20:19:20 -0700

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -4,3 +4,12 @@ import java.util.Random;
 
 //Class Numbers start
 public class Numbers {
+    private int randomNum;
+
+    public void generateNumber() 
+    {
+        Random rand = new Random();
+        randomNum = rand.nextInt(101);
+    }
+
+  
