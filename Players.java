8ca79d023dbbddf818139bdd908e43f8c351b92e
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-01-29 04:14:52 -0700

    Create Players.java

---
+++ Players.java
@@ -0,0 +1,42 @@
+
+
+
+
+public class Players extends Person {
+    private double money;
+  
+    public Players(String firstName, double money) {
+      super(firstName);
+      this.money = money;
+    }
+  
+    public Players(String firstName, String lastName, double money) {
+      super(firstName, lastName);
+      this.money = money;
+    }
+  
+    public double getMoney() {
+      return money;
+    }
+  
+    public void setMoney(double money) {
+      this.money = money;
+    }
+
+
+    public void increaseMoney(int amount) {
+        this.money += amount;
+    }
+    
+    public void decreaseMoney(int amount) {
+        this.money -= amount;
+    }
+    
+
+  
+    @Override
+    public String toString() {
+      return "Bat Man says " + super.getFirstName() + " " + super.getLastName() + ", Money: $" + money;
+    }
+  }
+  
