a75249d5c6137ead41b26a748ac7baefeba0af3f
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-05 19:57:55 -0700

    updated the players class
    refined from lesson 3

--- Players.java
+++ Players.java
@@ -1,42 +1,27 @@
 
-
-/* 
-
 public class Players extends Person {
-    private double money;
-  
-    public Players(String firstName, double money) {
+  private int money;
+
+  public Players(String firstName) {
       super(firstName);
-      this.money = money;
-    }
-  
-    public Players(String firstName, String lastName, double money) {
-      super(firstName, lastName);
-      this.money = money;
-    }
-  
-    public double getMoney() {
-      return money;
-    }
-  
-    public void setMoney(double money) {
-      this.money = money;
-    }
-
-
-    public void increaseMoney(int amount) {
-        this.money += amount;
-    }
-    
-    public void decreaseMoney(int amount) {
-        this.money -= amount;
-    }
-    
-
-  
-    @Override
-    public String toString() {
-      return "Bat Man says " + super.getFirstName() + " " + super.getLastName() + ", Money: $" + money;
-    }
+      this.money = 1000;
   }
-  */
+
+  public Players(String firstName, String lastName) {
+      super(firstName, lastName);
+      this.money = 1000;
+  }
+
+  public int getMoney() {
+      return money;
+  }
+
+  public void setMoney(int money) {
+      this.money = money;
+  }
+
+  @Override
+  public String toString() {
+      return super.toString() + ", current amount of money: " + this.money;
+  }
+}
