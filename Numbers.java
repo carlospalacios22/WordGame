8e156a6d7dcb23c484a25e996e8f34bca191aca2
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-05 19:48:59 -0700

    numbers started over from lesson 2

--- Numbers.java
+++ Numbers.java
@@ -1,51 +1,33 @@
-
-//using the Random library
 import java.util.Random;
 
-
-//////////////////////////////////////////
-
-//start of Numbers class
 public class Numbers {
-    private static int randomNum;
+  private int randomNum;
+  
+  public int getRandomNum() {
+    return randomNum;
+  }
+  
+  public void setRandomNum(int randomNum) {
+    this.randomNum = randomNum;
+  }
+  
+  public void generateNumber() {
+    Random random = new Random();
+    this.randomNum = random.nextInt(101);
+  }
+  
+  public boolean compareNumber(int guess) {
+    if (guess == this.randomNum) {
+      System.out.println("Congratulations, you guessed the number!");
+      return true;
+    } else if (guess > this.randomNum) {
+      System.out.println("I'm sorry.  That guess was too high.");
+      return false;
+    } else {
+      System.out.println("I'm sorry, That guess was too low.");
+      return false;
 
-    public void generateNumber() 
-    {
-        Random rand = new Random();
-        randomNum = rand.nextInt(101);
-    }
-
-    public boolean compareNumber(int guess) 
-    {
-        if (randomNum == guess) {
-            System.out.println("Congratulations, you guessed the number!");
-            return true;
-        } else if (randomNum < guess) {
-            System.out.println("I'm sorry. That guess was too high.");
-            return false;
-        } else {
-            System.out.println("I'm sorry, That guess was too low.");
-            return false;
-        }
-    }
-
-    public static int getRandomNum() 
-    {
-        return randomNum;
-    }
-
-    public static void setRandomNum(int randomNum) 
-    {
-        Numbers.randomNum = randomNum;
-    }
-
-    public int getNumToGuess() {
-        return randomNum;
-    }
-
-    public int getRandomNumber() {
-        return 0;
-    }
-    
-    
-}//end of numbers class
+      
+    }// End of if/else
+  }// End of compareNumber
+}// End of Numbers
