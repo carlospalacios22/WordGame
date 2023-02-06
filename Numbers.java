8ed8d6458ff1322de6429be74c632c2285634eae
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-05 20:01:50 -0700

    Update Numbers.java
     changed the randomNum field to static.

--- Numbers.java
+++ Numbers.java
@@ -1,33 +1,34 @@
 import java.util.Random;
 
-public class Numbers {
-  private int randomNum;
-  
-  public int getRandomNum() {
-    return randomNum;
-  }
-  
-  public void setRandomNum(int randomNum) {
-    this.randomNum = randomNum;
-  }
-  
-  public void generateNumber() {
-    Random random = new Random();
-    this.randomNum = random.nextInt(101);
-  }
-  
-  public boolean compareNumber(int guess) {
-    if (guess == this.randomNum) {
-      System.out.println("Congratulations, you guessed the number!");
-      return true;
-    } else if (guess > this.randomNum) {
-      System.out.println("I'm sorry.  That guess was too high.");
-      return false;
-    } else {
-      System.out.println("I'm sorry, That guess was too low.");
-      return false;
+import java.util.Random;
 
-      
-    }// End of if/else
-  }// End of compareNumber
-}// End of Numbers
+public class Numbers {
+  private static int randomNum;
+
+  public static int getRandomNum() {
+      return randomNum;
+  }
+
+  public static void setRandomNum(int randomNum) {
+      Numbers.randomNum = randomNum;
+  }
+
+  public static void generateNumber() {
+      Random random = new Random();
+      randomNum = random.nextInt(101);
+  }
+
+  public static boolean compareNumber(int guess) {
+      if (guess == randomNum) {
+          System.out.println("Congratulations, you guessed the number!");
+          return true;
+      } else if (guess > randomNum) {
+          System.out.println("I'm sorry. That guess was too high.");
+          return false;
+      } else {
+          System.out.println("I'm sorry, that guess was too low.");
+          return false;
+      }
+  }
+}
+
