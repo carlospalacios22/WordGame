909524e90829b872c6771cd57c221359d3b0146a
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-07 01:45:45 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -8,10 +8,14 @@ public class Turn {
       System.out.println("Rich Uncle Pennybags: " + player.getFirstName() + ", enter a guess for the random number between 0 and 100.");
       Scanner input = new Scanner(System.in);
       int guess = input.nextInt();
+
         // Create a new Numbers object
-      Numbers numbers = new Numbers();
+       Numbers numbers = new Numbers();
+       
       // Call the compareNumber method
       boolean correctGuess = numbers.compareNumber(guess);
+
+
         // If the guess is correct, add $500 to the player's money
       if (correctGuess) {
           player.setMoney(player.getMoney() + 500);
@@ -19,6 +23,8 @@ public class Turn {
         
           return true;
       } // end of if statement
+
+
 
       // If the guess is incorrect, subtract $200 from the player's money
       else {
