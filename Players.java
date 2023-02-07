a3ca1229d157af6a13e234f2d1f7233ceda7db5d
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-06 23:32:20 -0700

    Update Players.java

--- Players.java
+++ Players.java
@@ -1,27 +1,34 @@
-
 public class Players extends Person {
-  private int money;
+    private int money;
 
-  public Players(String firstName) {
-      super(firstName);
-      this.money = 1000;
-  }
+    public int getMoney() {
+        return money;
+    }
 
-  public Players(String firstName, String lastName) {
-      super(firstName, lastName);
-      this.money = 1000;
-  }
+    public void setMoney(int money) {
+        this.money = money;
+    }
 
-  public int getMoney() {
-      return money;
-  }
+    public Players() {
+        super(firstName);
+        this.money = 1000;
+    }
 
-  public void setMoney(int money) {
-      this.money = money;
-  }
+    public Players(String firstName, String lastName) {
+        super(firstName, lastName);
+        this.money = 1000;
+    }
 
-  @Override
-  public String toString() {
-      return super.toString() + ", current amount of money: " + this.money;
-  }
-}
+    public Players(String firstName) {
+        super(firstName);
+        this.money = 1000;
+    }
+//have the initial money be shown in the toString method not working
+
+    @Override
+    public String toString() {
+        return getFirstName() + " " + getLastName() + " now has $" + money + " dollars.";
+    }// end of toString method
+
+    
+}// end of Players class
