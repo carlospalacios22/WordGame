7f6649cf5a886a07cc94b1e58eda7d4291a129dd
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-06 23:30:35 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -32,5 +32,7 @@ public class Person {
 // added a toString method to return the last name
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-}
+
+        
+    }// end of setLastName method
+}// end of Person class
