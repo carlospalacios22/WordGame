8f48bdc7c872172d23a152a7a4daabf40bfe3f49
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-06 23:26:58 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,37 +1,32 @@
 public class Person {
-  
 
-  //first name and last name
-    private String firstName;
-    private String lastName;
-    
+
+    protected static String firstName; // changed to protected so that it can be accessed by the Players class
+    private String lastName; // changed to private so that it can only be accessed by the Person class
+
     public Person(String firstName) {
-      this.firstName = firstName;
-      this.lastName = "";
+        Person.firstName = firstName;
+        this.lastName = "";
     }
-    
+
     public Person(String firstName, String lastName) {
-      this.firstName = firstName;
-      this.lastName = lastName;
+        Person.firstName = firstName;
+        this.lastName = lastName;
     }
-    //getters and setters
+
     public String getFirstName() {
-      return firstName;
+        return firstName;
     }
-    
-    public String getLastName() {
-      return lastName;
-    }
-    
+
     public void setFirstName(String firstName) {
-      this.firstName = firstName;
+        Person.firstName = firstName;
     }
-    
+
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
-      this.lastName = lastName;
-
+        this.lastName = lastName;
     }
-
-    
-  }// End of Person
-  
+}
