63f1965fe645b29429150b70897ce3cb731d3642
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-06 23:36:00 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// Start of the Turn class
 public class Turn {
   public boolean takeTurn(Players player, Hosts host) {
 
@@ -8,17 +9,23 @@ public class Turn {
       Scanner input = new Scanner(System.in);
       int guess = input.nextInt();
       Numbers numbers = new Numbers();
+      // Call the compareNumber method
       boolean correctGuess = numbers.compareNumber(guess);
+        // If the guess is correct, add $500 to the player's money
       if (correctGuess) {
           player.setMoney(player.getMoney() + 500);
           System.out.println("Congratulations, " + player.getFirstName() + "! You are the winner.");
         
           return true;
-      } else {
+
+      } 
+      // If the guess is incorrect, subtract $200 from the player's money
+      else {
           player.setMoney(player.getMoney() - 200);
          
           System.out.println("Your guess was too " + (guess > Numbers.getRandomNum() ? "high." : "low."));
           return false;
-      }
-  }
-}
+
+      }// end of if/else statement
+  }// end of takeTurn method
+}// end of Turn class
