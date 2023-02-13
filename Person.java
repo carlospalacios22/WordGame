e84d06ee76351c77b2ec0474d922940455e9fcc4
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 00:32:29 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,17 +1,21 @@
+
+
+//  a person with a first name and a last name.
 public class Person {
     private String firstName;
     private String lastName;
-    
+    //  a person with a first name and a last name.
     public Person(String firstName) {
       this.firstName = firstName;
       this.lastName = "";
     }
-    
+  
     public Person(String firstName, String lastName) {
       this.firstName = firstName;
       this.lastName = lastName;
     }
     
+    // getters and setters
     public String getFirstName() {
       return firstName;
     }
@@ -26,6 +30,10 @@ public class Person {
     
     public void setLastName(String lastName) {
       this.lastName = lastName;
-    }
-  }
+    }// End of getters and setters
+
+
+  }// End of Person class
+
+
   
