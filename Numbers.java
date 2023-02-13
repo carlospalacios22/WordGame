078fd930984e36c21249815ca49b87c40c0a057a
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 00:31:17 -0700

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,31 +1,29 @@
 import java.util.Random;
 
+// Start of Numbers class
 public class Numbers {
-  private static int randomNum;
-  
-  public static int getRandomNum() {
-    return randomNum;
-  }
-  
-  public static void setRandomNum(int randomNum) {
-    Numbers.randomNum = randomNum;
-  }
-  
-  public void generateNumber() {
+  public static int randomNum;
+  // Start of generateNumber method
+  public static void generateNumber() {
     Random rand = new Random();
     randomNum = rand.nextInt(101);
   }
   
-  public boolean compareNumber(int guess) {
+  public static boolean compareNumber(int guess) {
+    // If the guess is correct, then return true
     if (guess == randomNum) {
       System.out.println("Congratulations, you guessed the number!");
       return true;
+      // If the guess is too high, then return false
     } else if (guess > randomNum) {
       System.out.println("I'm sorry. That guess was too high.");
       return false;
     } else {
+      // If the guess is too low, then return false
       System.out.println("I'm sorry, That guess was too low.");
       return false;
-    }
-  }
-}
+    }// End of if else statement
+
+
+  }// End of compareNumber method
+}// End of Numbers class
