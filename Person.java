658d30356a18aabfa99020f7d194b65f74ff1c2d
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-12 17:00:12 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,39 +1,31 @@
 public class Person {
-
-// made some changes to reduce the warnings suggestions by the IDE.
-
-    protected static String firstName; // changed to protected so that it can be accessed by the Players class
-    private String lastName; // changed to private so that it can only be accessed by the Person class
-
-    public Person(String firstName) 
-    {
-        Person.firstName = firstName;
-        this.lastName = "";// added a "blank" to lastName
+    private String firstName;
+    private String lastName;
+    
+    public Person(String firstName) {
+      this.firstName = firstName;
+      this.lastName = "";
     }
-
-
-// added a constructor to set the first and last name
+    
     public Person(String firstName, String lastName) {
-        Person.firstName = firstName;
-        this.lastName = lastName;
+      this.firstName = firstName;
+      this.lastName = lastName;
     }
-// added a toString method to return the first name
+    
     public String getFirstName() {
-        return firstName;
+      return firstName;
     }
-// added a toString method to return the first name
+    
     public void setFirstName(String firstName) {
-        Person.firstName = firstName;
+      this.firstName = firstName;
     }
-// added a toString method to return  and last name
+    
     public String getLastName() {
-        return lastName;
+      return lastName;
     }
-// added a toString method to return the last name
+    
     public void setLastName(String lastName) {
-        
-        this.lastName = lastName;
-
-        
-    }// end of setLastName method
-}// end of Person class
+      this.lastName = lastName;
+    }
+  }
+  
