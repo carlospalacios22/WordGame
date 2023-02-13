5e54c9c790d7c4f71ee15342d012fd9459ba2dcd
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 00:34:17 -0700

    Update Players.java

--- Players.java
+++ Players.java
@@ -1,14 +1,17 @@
+
+// begin of Players 
 public class Players extends Person {
     private int money;
-  
+
+  // getters and setters
     public int getMoney() {
       return money;
     }
   
     public void setMoney(int money) {
       this.money = money;
-    }
-  
+    }// end of getters and setters
+
     public Players(String firstName, String lastName) {
       super(firstName, lastName);
       this.money = 1000;
@@ -18,10 +21,12 @@ public class Players extends Person {
       super(firstName);
       this.money = 1000;
     }
-  
+
+  //override the toString method
     @Override
     public String toString() {
       return "Player: " + getFirstName() + " " + getLastName() + " - Current of money: $" + money;
     }
-  }
+
+  }// End of Players class
   
