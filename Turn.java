924aaa862df333d08b89db6a1d233302da537d24
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 01:00:18 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,35 +1,15 @@
-
-
-// import Scanner;
 import java.util.Scanner;
-// Start of Turn class
-public class Turn {
-  // Money amounts
-    private final int INCREMENT_AMOUNT = 500;
-    private final int DECREMENT_AMOUNT = 200;
-  // Start of takeTurn method
-    public boolean takeTurn(Players player, Hosts host) {
-      // Ask the player to guess a number
-      System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter your guess between 0 and 100: ");
-      Scanner input = new Scanner(System.in);
-      // Get the players guess
-      int guess = input.nextInt();
-      // Compare the players guess to the random number
-      boolean result = Numbers.compareNumber(guess);
-      // If the guess is correct, then add 500 to the players money
-      if (result) {
-        player.setMoney(player.getMoney() + INCREMENT_AMOUNT);
-        System.out.println("Congratulations! " + player.getFirstName() + " won $" + INCREMENT_AMOUNT);
-        System.out.println(player.toString());
-        return true;
-        // If the guess is incorrect, then subtract 200 from the players money
-      } else {
-        player.setMoney(player.getMoney() - DECREMENT_AMOUNT);
-        System.out.println(player.getFirstName() + " lost $" + DECREMENT_AMOUNT);
-        System.out.println(player.toString());
-        return false;
-      }// End of if else statement
 
-    }// End of takeTurn method
-    
-}// End of Turn class
+public class Turn {
+  public boolean takeTurn(Players player, Hosts host) {
+    System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter your guess between 0 and 100: ");
+    Scanner input = new Scanner(System.in);
+    int guess = input.nextInt();
+    boolean result = Numbers.compareNumber(guess);
+    Money money = new Money();
+    int winnings = money.displayWinnings(player, result);
+    player.setMoney(player.getMoney() + winnings);
+    System.out.println(player.toString());
+    return result;
+  }
+}
