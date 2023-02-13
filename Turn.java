ddcbdd832c5bcd5b2064cebc2d9125c75dbdafd1
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-12 17:01:02 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,38 +1,27 @@
 import java.util.Scanner;
 
-// Start of the Turn class
 public class Turn {
-  public boolean takeTurn(Players player, Hosts host) {
-
-        // Ask the user for a guess
-      System.out.println("Rich Uncle Pennybags: " + player.getFirstName() + ", enter a guess for the random number between 0 and 100.");
+    private final int INCREMENT_AMOUNT = 500;
+    private final int DECREMENT_AMOUNT = 200;
+  
+    public boolean takeTurn(Players player, Hosts host) {
+      host.randomizeNum();
+      Numbers numbers = new Numbers();
+      System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter your guess between 0 and 100: ");
       Scanner input = new Scanner(System.in);
       int guess = input.nextInt();
-
-        // Create a new Numbers object
-       Numbers numbers = new Numbers();
-       
-      // Call the compareNumber method
-      boolean correctGuess = numbers.compareNumber(guess);
-
-
-        // If the guess is correct, add $500 to the player's money
-      if (correctGuess) {
-          player.setMoney(player.getMoney() + 500);
-          System.out.println("Congratulations, " + player.getFirstName() + "! You are the winner.");
-        
-          return true;
-      } // end of if statement
-
-
-
-      // If the guess is incorrect, subtract $200 from the player's money
-      else {
-          player.setMoney(player.getMoney() - 200);
-         
-          System.out.println("Your guess was too " + (guess > Numbers.getRandomNum() ? "high." : "low."));
-          return false;
-
-      }// end of if/else statement
-  }// end of takeTurn method
-}// end of Turn class
+      boolean result = numbers.compareNumber(guess);
+      if (result) {
+        player.setMoney(player.getMoney() + INCREMENT_AMOUNT);
+        System.out.println("Congratulations! " + player.getFirstName() + " won $" + INCREMENT_AMOUNT);
+        System.out.println(player.toString());
+        return true;
+      } else {
+        player.setMoney(player.getMoney() - DECREMENT_AMOUNT);
+        System.out.println(player.getFirstName() + " lost $" + DECREMENT_AMOUNT);
+        System.out.println(player.toString());
+        return false;
+      }
+    }
+  }
+  
