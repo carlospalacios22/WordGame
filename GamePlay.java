42e9b621ed771370df5458f1dfdf8a9ce126a20c
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-12 17:10:03 -0700

    Final commit.

--- GamePlay.java
+++ GamePlay.java
@@ -1,56 +1,66 @@
 
-//Import Scanner library for input/outputs
+
+//importing the Scanner class
 import java.util.Scanner;
 
-//Start of GamePlay
+
+
+//Beginning of GamePlay class
+
+
 public class GamePlay {
-    private Players player;
-    private Hosts host;
+  private Players player;
+//beginning of main method
+  public static void main(String[] args) {
+    //creating a new Scanner object
+    Scanner input = new Scanner(System.in);
+    GamePlay game = new GamePlay();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        GamePlay game = new GamePlay();
+    //creating a new Hosts object
+    Hosts host = new Hosts("Pennybags","");
+    host.randomizeNum();
+    Turn turn = new Turn();
+//prompting the user to enter their name
+    System.out.print("Enter your name: ");
+    String name = input.nextLine();
+//prompting the user to enter their last name
+    System.out.print("Would you like to enter a last name? (yes/no): ");
+    String answer = input.nextLine();
+    //if the user enters yes, then they will be prompted to enter their last name
+    if (answer.equals("yes")) {
+      System.out.print("Enter your last name: ");
+      String lastName = input.nextLine();
+      game.player = new Players(name, lastName);
 
-        game.host = new Hosts();
-        game.host.randomizeNum();
-
-        System.out.print("What is your first name? ");
-        String firstName = sc.nextLine();
-        
-           // Prompt for the last name
-        //if user enters y then get the last name.
-        System.out.print("Would you like to enter a last name? (y/n) ");
-        String lastNameResponse = sc.nextLine();
-        if (lastNameResponse.equalsIgnoreCase("y")) {
-            System.out.print("What is your last name? ");
-            String lastName = sc.nextLine();
-            game.player = new Players(firstName, lastName, 0.0);
-        } else {
-            game.player = new Players(firstName, 0.0);
-        }
-        
-        Turn turn = new Turn();
-
-        // loop that allows the player to keep playing or stop
-        boolean keepPlaying = true;
-        while (keepPlaying) {
-            boolean correctGuess = false;
-            while (!correctGuess) {
-                correctGuess = turn.takeTurn(game.player);
-            }
-            
-            System.out.print("Do you want to keep playing? (y/n) ");
-            String response = sc.nextLine();
-            if (response.equalsIgnoreCase("n")) {
-                keepPlaying = false;
-            } else {
-                game.host.randomizeNum();
-            }
-        }//end of game loop
-
-
-    }//end of void main
+    } else {
+      //if the user enters no, then they will not be prompted to enter their last name
+      game.player = new Players(name);
+    }
+//setting the player's money to 1000
+    game.player.setMoney(1000);
 
 
 
-}//Numbers class ends 
+    boolean playAgain = true;
+    while (playAgain) {
+      while (!turn.takeTurn(game.player, host)) {
+
+      
+      }
+      //prompting the user to play again
+      System.out.println("Would you like to play again? (yes/no): ");
+      answer = input.nextLine();
+
+      //if the user enters yes, then the game will restart
+      if (answer.equals("no")) {
+        playAgain = false;
+
+        //if the user enters no, then the game will end
+      } else {
+        host.randomizeNum();
+      }// end of if else
+
+
+    }// end of while loop
+  }// end of main method
+}// enmd of GamePlay class
