be6e5d7ff5c71a917a9c2c31249091fc58883010
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-12 17:00:40 -0700

    Update Players.java

--- Players.java
+++ Players.java
@@ -1,38 +1,27 @@
 public class Players extends Person {
     private int money;
-
+  
     public int getMoney() {
-        return money;
+      return money;
     }
-
+  
     public void setMoney(int money) {
-        this.money = money;
+      this.money = money;
     }
-
-    public Players() {
-        super(firstName);
-        this.money = 1000;
-    }
-
+  
     public Players(String firstName, String lastName) {
-        super(firstName, lastName);
-        this.money = 1000;
+      super(firstName, lastName);
+      this.money = 1000;
     }
-
+  
     public Players(String firstName) {
-        super(firstName);
-        this.money = 1000;
+      super(firstName);
+      this.money = 1000;
     }
-
-
+  
     @Override
     public String toString() {
-        return getFirstName() + " " + getLastName() + " now has $" + money + " dollars.";
-    }// end of toString method
-
-    public String getCurrentUserName() {
-        return null;
+      return "Player: " + getFirstName() + " " + getLastName() + " - Current of money: $" + money;
     }
-
-    
-}// end of Players class
+  }
+  
