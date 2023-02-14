5e00debf2366e8e1ac619d76b1e0636610bfa5ba
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 22:27:05 -0700

    Update Phrases.java

--- Phrases.java
+++ Phrases.java
@@ -2,27 +2,22 @@ import java.util.Random;
 
 public class Phrases {
   public static String randomPhrase;
-  // Start of generatePhrase method
+  public static String playingPhrase;
+
   public static void generatePhrase() {
-    String[] phrases = {"hello", "goodbye", "good morning", "good afternoon", "good evening"};
+    String[] phrases = {
+      "It's not rocket science",
+      "Kill two birds with one stone",
+      "Piece of cake",
+      "Let the cat out of the bag",
+      "Barking up the wrong tree"
+    };
     Random rand = new Random();
     randomPhrase = phrases[rand.nextInt(phrases.length)];
+    playingPhrase = randomPhrase.replaceAll("[a-zA-Z]", "_");
   }
-  
-  public static boolean comparePhrase(String guess) throws MultipleLettersException {
-    // If the guess is correct, then return true
-    if (guess.equals(randomPhrase)) {
-      System.out.println("Congratulations, you guessed the phrase!");
-      return true;
-      // If the guess contains more than one letter, throw a MultipleLettersException
-    } else if (guess.length() > 1) {
-      throw new MultipleLettersException("More than one letter was entered");
-    } else {
-      // If the guess is incorrect but only contains one letter, return false
-      System.out.println("I'm sorry, that guess was incorrect.");
 
-      
-      return false;
-    }// End of if else statement
-  }// End of comparePhrase method
-}// End of Phrases class
+  public static String getPhrase() {
+    return playingPhrase;
+  }
+}
