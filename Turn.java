befa2b73994e3d49d766f2136c65ce0213f4e847
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 23:45:37 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,15 +1,48 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Turn {
   public boolean takeTurn(Players player, Hosts host) {
-    System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter your guess between 0 and 100: ");
     Scanner input = new Scanner(System.in);
-    int guess = input.nextInt();
-    boolean result = Numbers.compareNumber(guess);
-    Money money = new Money();
-    int winnings = money.displayWinnings(player, result);
-    player.setMoney(player.getMoney() + winnings);
-    System.out.println(player.toString());
-    return result;
+
+    // Ask the player to guess a letter
+    System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter: ");
+    String guessStr = input.nextLine().toLowerCase();
+    char guess = guessStr.charAt(0);
+
+    // Check if the guess is a letter
+    if (!Character.isLetter(guess)) {
+      System.out.println("Invalid input. Please enter a letter.");
+      return false;
+    }
+
+    try {
+      // Evaluate the guess using the findLetters method from the Phrases class
+      boolean result = Phrases.findLetters(guess);
+
+      // If the guess is correct, then display the winnings
+      Random random = new Random();
+      int prizeType = random.nextInt(2);
+      int winnings = 0;
+
+      // If the prize type is 0, then it is money
+      if (prizeType == 0) {
+        Money money = new Money();
+        winnings = money.displayWinnings(player, result);
+
+      // If the prize type is 1, then it is physical
+      } else {
+        Physical physical = new Physical();
+        winnings = physical.displayWinnings(player, result);
+      }
+
+      player.setMoney(player.getMoney() + winnings);
+      System.out.println(player.toString());
+      return result;
+    } catch (MultipleLettersException e) {
+      System.out.println(e.getMessage());
+    }
+
+    return false;
   }
 }
