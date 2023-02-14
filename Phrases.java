49fd6f3274b483f3ca90a9c0d4d88fb77706423c
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-13 22:45:26 -0700

    Update Phrases.java

--- Phrases.java
+++ Phrases.java
@@ -1,48 +1,40 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Phrases {
-  public static String gamePhrase;
-  public static String playingPhrase;
-  public static ArrayList<Integer> indices;
-
-  public static void generatePhrase() {
-    String[] phrases = {
-      "It's not rocket science",
-      "Kill two birds with one stone",
-      "Piece of cake",
-      "Let the cat out of the bag",
-      "Barking up the wrong tree"
-    };
-    Random rand = new Random();
-    gamePhrase = phrases[rand.nextInt(phrases.length)];
-    playingPhrase = gamePhrase.replaceAll("[a-zA-Z]", "_");
-  }
-
-  public static String getPhrase() {
-    return playingPhrase;
-  }
-
-  public static void findLetters(String letter) throws MultipleLettersException {
-    if (letter.length() != 1) {
-      throw new MultipleLettersException();
+    public static String gamePhrase;
+    public static String playingPhrase;
+    public static ArrayList<Integer> indices;
+  
+    public static void setGamePhrase(String phrase) {
+      gamePhrase = phrase;
+      playingPhrase = gamePhrase.replaceAll("[a-zA-Z]", "_");
     }
-    indices = new ArrayList<>();
-    int index = gamePhrase.indexOf(letter);
-    while (index >= 0) {
-      indices.add(index);
-      index = gamePhrase.indexOf(letter, index + 1);
+  
+    public static String getPhrase() {
+      return playingPhrase;
     }
-    if (indices.size() > 0) {
-      char[] charArray = playingPhrase.toCharArray();
-      for (int i = 0; i < indices.size(); i++) {
-        charArray[indices.get(i)] = letter.charAt(0);
+  
+    public static void findLetters(String letter) throws MultipleLettersException {
+      if (letter.length() != 1) {
+        throw new MultipleLettersException();
       }
-      playingPhrase = new String(charArray);
-      if (!playingPhrase.contains("_")) {
-        System.out.println("Congratulations, you won!");
-        // award a prize here
+      indices = new ArrayList<>();
+      int index = gamePhrase.indexOf(letter);
+      while (index >= 0) {
+        indices.add(index);
+        index = gamePhrase.indexOf(letter, index + 1);
+      }
+      if (indices.size() > 0) {
+        char[] charArray = playingPhrase.toCharArray();
+        for (int i = 0; i < indices.size(); i++) {
+          charArray[indices.get(i)] = letter.charAt(0);
+        }
+        playingPhrase = new String(charArray);
+        if (!playingPhrase.contains("_")) {
+          System.out.println("Congratulations, you won!");
+          // award a prize here
+        }
       }
     }
   }
-}
+  
