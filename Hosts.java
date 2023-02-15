1f1c918392345f099700aa98a1fdfc4bcbcaa522
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-14 23:34:47 -0700

    Update Hosts.java

--- Hosts.java
+++ Hosts.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class Hosts extends Person {
+    private String gamePhrase;
 
     public Hosts(String firstName, String lastName) {
         super(firstName, lastName);
+        this.gamePhrase = "";
     }
 
     public void enterPhrase() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the phrase for the players to guess: ");
-        String phrase = scanner.nextLine();
-        Phrases.setGamePhrase(phrase);
-    
-        System.out.println("Do you want to enter a new phrase? (Y/N)");
-        String answer = scanner.nextLine();
-        if (answer.equalsIgnoreCase("Y")) {
-            enterPhrase();
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.println(getFirstName() + ", please enter a phrase for the players to guess:");
+        this.gamePhrase = input.nextLine();
+        Phrases.setGamePhrase(gamePhrase);
+    }
+
+    public void randomizePhrase() {
+        enterPhrase();
     }
 }
