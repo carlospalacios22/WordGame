b1710dad864909052877fd6425cee2eac15739ea
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-16 02:36:55 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -4,15 +4,28 @@ public class Turn {
     public boolean takeTurn(Players player, Hosts host) {
         System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter:");
         Scanner input = new Scanner(System.in);
-        String letter = input.nextLine();
+        String guess = input.nextLine();
+
+        boolean foundLetter = false;
         try {
-            Phrases.findLetters(letter);
+            foundLetter = Phrases.findLetters(guess);
         } catch (MultipleLettersException e) {
             System.out.println(e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Please enter a letter, not a number or symbol");
         }
-        System.out.println(Phrases.getPlayingPhrase());
-        return !Phrases.getPlayingPhrase().contains("_");
+
+        if (foundLetter) {
+            int winnings = new Money().displayWinnings(player, true);
+            player.setMoney(player.getMoney() + winnings);
+            System.out.println(player.toString());
+        } else {
+            System.out.println(Phrases.getPlayingPhrase());
+        }
+
+        if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
+            System.out.println("Congratulations, you won the game!");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
