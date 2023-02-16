85ae1c7f897a710b2bd74a147cab25de803ccede
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-16 02:32:59 -0700

    Update Phrases.java

--- Phrases.java
+++ Phrases.java
@@ -1,28 +1,42 @@
+
+
+
+
+// Start of Phrases class
 public class Phrases {
+
+    // instance variables
     private static String gamePhrase;
     private static String playingPhrase;
 
+
+//Start of setGamePhrase method
     public static void setGamePhrase(String phrase) {
         gamePhrase = phrase;
-        playingPhrase = gamePhrase.replaceAll("[A-Za-z]", "_");
+        // replaces all the letters in the phrase with underscores
+        playingPhrase = gamePhrase.replaceAll("[a-zA-Z]", "_");
     }
-
+//Start of getPlayingPhrase method
     public static String getPlayingPhrase() {
         return playingPhrase;
     }
 
-    public static void findLetters(String letter) throws MultipleLettersException {
-        if (letter.length() > 1) {
+//Start of findLetters method
+    public static boolean findLetters(String letter) throws MultipleLettersException {
+        // if statement to check if the letter is more than one character
+        if (letter.length() != 1) {
             throw new MultipleLettersException();
         }
+        boolean foundLetter = false;
+        // for loop to find the letters in the phrase
         for (int i = 0; i < gamePhrase.length(); i++) {
-            if (gamePhrase.charAt(i) == letter.charAt(0)) {
-                playingPhrase = playingPhrase.substring(0, i) + letter + playingPhrase.substring(i + 1);
+            if (Character.toLowerCase(gamePhrase.charAt(i)) == letter.charAt(0)) {
+                playingPhrase = playingPhrase.substring(0, i) + gamePhrase.charAt(i) + playingPhrase.substring(i + 1);
+                foundLetter = true;
             }
         }
-        if (!playingPhrase.contains("_")) {
-            System.out.println("Congratulations, you won!");
-            // award prize
-        }
-    }
-}
+        return foundLetter;
+
+
+    }//End of findLetters method
+}//End of Phrases class
