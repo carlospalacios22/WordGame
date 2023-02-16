8f520b72956f5e09a9b1d1f3cd850f5a90ffc2c3
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-16 02:41:27 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,13 +1,22 @@
+
+
+
 import java.util.Scanner;
 
+
+//Start of Turn class
 public class Turn {
+    //Start of takeTurn method
     public boolean takeTurn(Players player, Hosts host) {
+
+        //Start of try/catch block
         System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter:");
         Scanner input = new Scanner(System.in);
         String guess = input.nextLine();
 
         boolean foundLetter = false;
         try {
+            
             foundLetter = Phrases.findLetters(guess);
         } catch (MultipleLettersException e) {
             System.out.println(e.getMessage());
@@ -20,12 +29,14 @@ public class Turn {
         } else {
             System.out.println(Phrases.getPlayingPhrase());
         }
-
+    
         if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
             System.out.println("Congratulations, you won the game!");
             return true;
         } else {
             return false;
-        }
-    }
-}
+
+
+        }//End of if statement
+    }//End of takeTurn method
+}//End of Turn class
