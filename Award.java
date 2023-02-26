6b411d57f1b0d011fe53eb82e2c0479dd24b2a63
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 04:26:05 -0700

    Update Award.java

--- Award.java
+++ Award.java
@@ -10,13 +10,9 @@
 
 // This interface  to display the winnings of the player
 public interface Award {
+
+    
     public int displayWinnings(Players player, boolean isGuessCorrect);
-    }
-
-
-
-
-
-
+    }//end of interface Award
 
 
