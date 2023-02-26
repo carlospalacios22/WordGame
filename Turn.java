db287d40753d145a13dde2e6b873e0b20f411b2a
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 04:43:38 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -1,42 +1,44 @@
+import javax.swing.*;
 
-
-
-import java.util.Scanner;
-
-
-//Start of Turn class
 public class Turn {
-    //Start of takeTurn method
+    
+    private GUI gui;
+    
+    public Turn(GUI gui) {
+        this.gui = gui;
+    }
+
     public boolean takeTurn(Players player, Hosts host) {
-
-        //Start of try/catch block
-        System.out.println(host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter:");
-        Scanner input = new Scanner(System.in);
-        String guess = input.nextLine();
-
-        boolean foundLetter = false;
-        try {
-            
-            foundLetter = Phrases.findLetters(guess);
-        } catch (MultipleLettersException e) {
-            System.out.println(e.getMessage());
-        }
-
-        if (foundLetter) {
-            int winnings = new Money().displayWinnings(player, true);
-            player.setMoney(player.getMoney() + winnings);
-            System.out.println(player.toString());
-        } else {
-            System.out.println(Phrases.getPlayingPhrase());
+        while (true) {
+            String guess = JOptionPane.showInputDialog(gui, host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter:");
+            if (guess == null) {
+                return false;
+            }
+    
+            boolean foundLetter = false;
+            try {
+                foundLetter = Phrases.findLetters(guess);
+            } catch (MultipleLettersException e) {
+                JOptionPane.showMessageDialog(gui, e.getMessage());
+            }
+    
+            if (foundLetter) {
+                int winnings = new Money().displayWinnings(player, true);
+                player.setMoney(player.getMoney() + winnings);
+                JOptionPane.showMessageDialog(gui, player.toString());
+            } else {
+                JOptionPane.showMessageDialog(gui, Phrases.getPlayingPhrase());
+                break;
+            }
+    
+            if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
+                JOptionPane.showMessageDialog(gui, "Congratulations, you won the game!");
+                return true;
+            }
         }
     
-        if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
-            System.out.println("Congratulations, you won the game!");
-            return true;
-        } else {
-            return false;
-
-
-        }//End of if statement
-    }//End of takeTurn method
-}//End of Turn class
+        return false;
+    }
+    
+    
+}
