3db7939b588154deea0e6a8f0ea36185ef4fed87
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 04:47:07 -0700

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -8,43 +8,50 @@ import javax.swing.*;
 public class Turn {
     
     private GUI gui;
-    
+    //  Turn constructor
     public Turn(GUI gui) {
         this.gui = gui;
     }
 
+    // Start of takeTurn method
     public boolean takeTurn(Players player, Hosts host) {
         while (true) {
+            // get the guess from the player
             String guess = JOptionPane.showInputDialog(gui, host.getFirstName() + ": " + player.getFirstName() + ", please enter a letter:");
+            
             if (guess == null) {
                 return false;
             }
-    
+    // check if the player entered a letter or return exception
             boolean foundLetter = false;
             try {
                 foundLetter = Phrases.findLetters(guess);
             } catch (MultipleLettersException e) {
                 JOptionPane.showMessageDialog(gui, e.getMessage());
             }
+            // check if the player won
     
             if (foundLetter) {
                 int winnings = new Money().displayWinnings(player, true);
                 player.setMoney(player.getMoney() + winnings);
                 JOptionPane.showMessageDialog(gui, player.toString());
             } else {
+                // display the phrase with the letters guessed so far (bugged)
                 JOptionPane.showMessageDialog(gui, Phrases.getPlayingPhrase());
                 break;
             }
-    
+    // check if the players won 
             if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
                 JOptionPane.showMessageDialog(gui, "Congratulations, you won the game!");
                 return true;
-            }
-        }
+            }// end of if
+
+
+        }// end of while loop
     
         return false;
 
-        
+
     }// End of takeTurn method
     
     
