41a20ac70ea78c33c20f0ab31a8e5c09720e5061
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 01:03:26 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,158 +1,158 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JFrame {
-    
-    // instance variables
-    private GamePlay game;
-    private Hosts host;
-    private Players[] players;
+    private Players[] players = new Players[3];
+    private Hosts host = new Hosts("Pennybags", "");
     private JLabel playersLabel;
     private JLabel hostLabel;
     private JLabel phraseLabel;
+    private JButton addPlayerButton;
+    private JButton addHostButton;
+    private JButton startButton;
     
-    // constructor
     public GUI() {
-        // create the game, host, and players
-        game = new GamePlay();
-        host = new Hosts("Pennybags", "");
-        players = new Players[3];
-        
-        // set up the main window
-        setTitle("Word Guessing Game");
-        setSize(600, 400);
+        super("Word Guessing Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 300));
         setLayout(new BorderLayout());
         
-        // create the UI components
-        playersLabel = new JLabel("Players: None");
-        hostLabel = new JLabel("Host: " + host.getFirstName());
-        phraseLabel = new JLabel(Phrases.getPlayingPhrase());
-        JButton addPlayerButton = new JButton("Add Player");
-        JButton addHostButton = new JButton("Add Host");
-        JButton startButton = new JButton("Start Game");
-        
-        // add the UI components to the window
+        // create a panel for the top section of the window
         JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+        
+        // create a label for the players
+        playersLabel = new JLabel("Players:");
         topPanel.add(playersLabel);
-        topPanel.add(addPlayerButton);
+        
+        // create a label for the host
+        hostLabel = new JLabel("Host: " + host.getFirstName());
         topPanel.add(hostLabel);
-        topPanel.add(addHostButton);
+        
         add(topPanel, BorderLayout.NORTH);
         
+        // create a panel for the center section of the window
         JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        
+        // create a label for the playing phrase
+        phraseLabel = new JLabel(Phrases.getPlayingPhrase());
         centerPanel.add(phraseLabel);
+        
+        // create a button to start the game
+        startButton = new JButton("Start Game");
+        startButton.addActionListener(new StartButtonListener());
+        centerPanel.add(startButton);
+        
         add(centerPanel, BorderLayout.CENTER);
         
+        // create a panel for the bottom section of the window
         JPanel bottomPanel = new JPanel();
-        bottomPanel.add(startButton);
+        
+        // create a button to add a player
+        addPlayerButton = new JButton("Add Player");
+        addPlayerButton.addActionListener(new AddPlayerButtonListener());
+        bottomPanel.add(addPlayerButton);
+        
+        // create a button to add a host
+        addHostButton = new JButton("Add Host");
+        addHostButton.addActionListener(new AddHostButtonListener());
+        bottomPanel.add(addHostButton);
+        
         add(bottomPanel, BorderLayout.SOUTH);
         
-        // add event listeners to the buttons
-        addPlayerButton.addActionListener(new AddPlayerListener());
-        addHostButton.addActionListener(new AddHostListener());
-        startButton.addActionListener(new StartGameListener());
+        pack();
+        setLocationRelativeTo(null);
     }
     
-    // inner class to handle adding a new player
-    private class AddPlayerListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            // prompt the user to enter the player's name
-            String name = JOptionPane.showInputDialog("Enter player name:");
-            
-            // create a new player with the entered name
-            Players player = new Players(name);
-            
-            // add the player to the players array
+    // listener for the add player button
+    private class AddPlayerButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             for (int i = 0; i < players.length; i++) {
                 if (players[i] == null) {
-                    players[i] = player;
+                    String name = JOptionPane.showInputDialog("Enter player " + (i + 1) + " name:");
+                    if (name != null && !name.trim().equals("")) {
+                        players[i] = new Players(name);
+                        updatePlayersLabel();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    // listener for the add host button
+    private class AddHostButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String name = JOptionPane.showInputDialog("Enter host name:");
+            if (name != null && !name.trim().equals("")) {
+                host = new Hosts(name, "");
+                host.enterPhrase();
+                hostLabel.setText("Host: " + host.getFirstName());
+            }
+        }
+    }
+    
+    // listener for the start game button
+    private class StartButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (host.getGamePhrase() == null || host.getGamePhrase().trim().equals("")) {
+                JOptionPane.showMessageDialog(GUI.this, "Please enter a game phrase.");
+                return;
+            }
+            
+            boolean hasPlayers = false;
+            for (int i = 0; i < players.length; i++) {
+                if (players[i] != null) {
+                    hasPlayers = true;
                     break;
                 }
             }
-            
-            // update the players label
-            updatePlayersLabel();
-        }
-    }
-    
-    // inner class to handle adding a new host
-    private class AddHostListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            // prompt the user to enter the host's name
-            String name = JOptionPane.showInputDialog("Enter host name:");
-            
-            // create a new host with the entered name
-            host = new Hosts(name, "");
-            
-            // update the host label
-            hostLabel.setText("Host: " + host.getFirstName());
-        }
-    }
-    
-    // inner class to handle starting the game
-    private class StartGameListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            // check if there are any players or a game phrase
-            if (players[0] == null || Phrases.getPlayingPhrase().equals("")) {
-                JOptionPane.showMessageDialog(null, "Error: No players or game phrase.");
+            if (!hasPlayers) {
+                JOptionPane.showMessageDialog(GUI.this, "Please add at least one player.");
                 return;
             }
             
             // start the game
-            boolean playAgain = true;
-            while (playAgain) {
-                int currentPlayerIndex = 0;
-                while (!game.takeTurn(players[currentPlayerIndex % 3],host)) {
-                    currentPlayerIndex++;
-                }
-                
-                // show the result of the turn in a message dialog
-                Players currentPlayer = players[currentPlayerIndex % 3];
-                String message = "";
-                int winnings = 0;
-                if (Phrases.getPlayingPhrase().indexOf('_') == -1) {
-                    message = "Congratulations, " + currentPlayer.getFirstName() + "! You won the game!";
-                } else {
-                    message = "Sorry, " + currentPlayer.getFirstName() + ", your guess was incorrect.";
-                }
-                winnings = currentPlayer.getMoney();
-                JOptionPane.showMessageDialog(null, message + " Your current winnings: $" + winnings);
-                
-                // prompt the user to play again
-                int playAgainChoice = JOptionPane.showConfirmDialog(null, "Do you want to play again?", "Play Again", JOptionPane.YES_NO_OPTION);
-                playAgain = (playAgainChoice == JOptionPane.YES_OPTION);
-                
-                if (playAgain) {
-                    // reset the game and host
-                    game = new GamePlay();
-                    host.enterPhrase();
-                    // clear the players array
-                    players = new Players[3];
-                    updatePlayersLabel();
-                    // reset the phrase label
-                    phraseLabel.setText(Phrases.getPlayingPhrase());
-                }
+            Turn turn = new Turn();
+            int currentPlayerIndex = 0;
+            while (!turn.takeTurn(players[currentPlayerIndex % 3], host)) {
+                currentPlayerIndex++;
+            }
+            int answer = JOptionPane.showConfirmDialog(GUI.this, "Would you like to play again?", "Play Again?", JOptionPane.YES_NO_OPTION);
+            if (answer == JOptionPane.YES_OPTION) {
+                // reset the game
+                players = new Players[3];
+                updatePlayersLabel();
+                host = new Hosts(host.getFirstName(), "");
+                host.enterPhrase();
+                hostLabel.setText("Host: " + host.getFirstName());
+                phraseLabel.setText(Phrases.getPlayingPhrase());
+            } else {
+                System.exit(0);
             }
         }
     }
     
     // helper method to update the players label
     private void updatePlayersLabel() {
-        String labelText = "Players:";
+        String playersText = "Players: ";
         for (int i = 0; i < players.length; i++) {
             if (players[i] != null) {
-                labelText += " " + players[i].getFirstName();
+                playersText += players[i].getFirstName() + " - Current money: $" + players[i].getMoney() + " ";
             }
         }
-        playersLabel.setText(labelText);
+        playersLabel.setText(playersText);
     }
     
-    // main method to create and show the GUI
+    // main method to start the application
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        gui.setVisible(true);
+        // start the EDT
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
     }
-}
+}    
