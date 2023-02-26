6483f02522977aeac3a7e32c4eb0bc79d91534d0
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 04:42:55 -0700

    Update Physical.java

--- Physical.java
+++ Physical.java
@@ -2,32 +2,30 @@
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Physical implements Award {
-// Array of physical prizes
-private String[] prizes = {"A trip to Hawaii", "A brand new car", "A year's supply of chocolate", "A 65-inch TV", "A luxury cruise"};
-// Method to get a random prize
-public int getRandomPrize() {
-Random rand = new Random();
+    // Array of physical prizes
+    private String[] prizes = {"A trip to Hawaii", "A brand new car", "A year's supply of chocolate", "A 65-inch TV", "A luxury cruise"};
+    private final int PRIZE_INDEX;
+    private static final int WINNINGS = 0;
 
-int randomIndex = rand.nextInt(prizes.length);
-return randomIndex;
+    // Constructor to get a random prize
+    public Physical() {
+        Random rand = new Random();
+        PRIZE_INDEX = rand.nextInt(prizes.length);
+    }
+
+    // Overridden method to display the winnings
+    @Override
+    public int displayWinnings(Players player, boolean isGuessCorrect) {
+        if (isGuessCorrect) {
+            // If the guess is correct, then display the "won prize"
+            JOptionPane.showMessageDialog(null, "Congratulations " + player.getFirstName() + "! You won " + prizes[PRIZE_INDEX]);
+        } else {
+            // If the guess is incorrect, then display the "could have won prize"
+            JOptionPane.showMessageDialog(null, "Unfortunately " + player.getFirstName() + ", you lost. You could have won " + prizes[PRIZE_INDEX]);
+        }
+        return WINNINGS;
+    }
 }
-
-// Overde Method to display the winnings
-@Override
-
-public int displayWinnings(Players player, boolean isGuessCorrect) {
-    // Get a random prize
-int randomPrizeIndex = getRandomPrize();
-if (isGuessCorrect) {
-    // If the guess is correct, then display the "won prize"
-System.out.println("Congratulations " + player.getFirstName() + "! You won " + prizes[randomPrizeIndex]);
-return 0;
-// If the guess is incorrect, then display the "could have won prize"
-} else {
-System.out.println("Unfortunately " + player.getFirstName() + ", you lost. You could have won " + prizes[randomPrizeIndex]);
-return 0;
-}// End of if-else
-
-}// End of displayWinnings method
-}// End of Physical class
