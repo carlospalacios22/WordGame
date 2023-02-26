79d626cf07fe206bcfc17e7f37b49fb7a088af62
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-02-26 04:41:18 -0700

    Update Money.java

--- Money.java
+++ Money.java
@@ -1,23 +1,25 @@
 
 
 
+
+
+import javax.swing.JOptionPane;
+
 public class Money implements Award {
-  private final int INCREMENT_AMOUNT = 500;
-  private final int DECREMENT_AMOUNT = 200;
+    private final int INCREMENT_AMOUNT = 500;
+    private final int DECREMENT_AMOUNT = 200;
 
-  //Start of displayWinnings method
-  @Override
-  public int displayWinnings(Players player, boolean correctGuess) {
-      if (correctGuess) {
-          System.out.println(player.getFirstName() + " won $" + INCREMENT_AMOUNT);
-          player.addWinnings(INCREMENT_AMOUNT);
-          return INCREMENT_AMOUNT;
-      } else {
-          System.out.println(player.getFirstName() + " lost $" + DECREMENT_AMOUNT);
-          player.addLosses(DECREMENT_AMOUNT);
-          return -DECREMENT_AMOUNT;
-
-
-      }//End of if statement
-  }//End of displayWinnings method
-}//End of Money class
+    // Start of displayWinnings method
+    @Override
+    public int displayWinnings(Players player, boolean correctGuess) {
+        if (correctGuess) {
+            JOptionPane.showMessageDialog(null, player.getFirstName() + " won $" + INCREMENT_AMOUNT);
+            player.addWinnings(INCREMENT_AMOUNT);
+            return INCREMENT_AMOUNT;
+        } else {
+            JOptionPane.showMessageDialog(null, player.getFirstName() + " lost $" + DECREMENT_AMOUNT);
+            player.addLosses(DECREMENT_AMOUNT);
+            return -DECREMENT_AMOUNT;
+        }
+    }// End of displayWinnings method
+}// End of Money class
